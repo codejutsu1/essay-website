69e6bf4a040b076c9f4af4e57363b7c673a3703e
codejutsu1 <codejutsu@protonmail.com>
2022-07-14 13:47:58 +0100

    change the role on the redirectifauthenticated

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -26,7 +26,7 @@ class RedirectIfAuthenticated
                 return redirect(RouteServiceProvider::ADMIN);
             }elseif(Auth::guard($guard)->check() && Auth::user()->role_id == 2) {
                 return redirect(RouteServiceProvider::USER);
-            }elseif(Auth::guard($guard)->check() && Auth::user()->role_id == 2){
+            }elseif(Auth::guard($guard)->check() && Auth::user()->role_id == 3){
                 return redirect(RouteServiceProvider::WRITER);
             }else{
                 return $next($request);
